123bf2ecad6ddf590c579505517d935ad0c0a212
Gary Yau <gary.yau@hotmail.com>
2022-05-25 00:42:50 +0900

    Added divs for left and right side

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-
+        <div className={styles.container}>
+          <div className={styles.left}>Left</div>
+          <div className={styles.right}>Right</div>
+        </div>
       </main>
     </div>
   )
