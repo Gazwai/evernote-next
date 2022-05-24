5d47c859fa2bcdff22d41d5539831a24d08eb634
Gary Yau <gary.yau@hotmail.com>
2022-05-25 00:57:21 +0900

    added NoteOperation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Evernote.module.scss'
+import NoteOperations from './components/NoteOperations';
+
 
 export default function Home() {
   return (
@@ -12,7 +14,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <div className={styles.container}>
-          <div className={styles.left}>Left</div>
+          <div className={styles.left}>
+            <NoteOperations />
+          </div>
           <div className={styles.right}>Right</div>
         </div>
       </main>
