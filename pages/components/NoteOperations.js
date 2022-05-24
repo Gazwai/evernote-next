df9d81eed564676105b3843cf38e85db3131ed82
Gary Yau <gary.yau@hotmail.com>
2022-05-25 01:02:30 +0900

    create a button to add new notes

--- pages/components/NoteOperations.js
+++ pages/components/NoteOperations.js
@@ -1,6 +1,14 @@
+import styles from '../../styles/Evernote.module.scss';
+
 export default function NoteOperations() {
-    return (
-        <>
-        </>
-    )
+  return (
+    <>
+      <div className='{styles.btnContainer}'>
+        <button
+          className={styles.button}>
+            Add a New Note
+          </button>
+      </div>
+    </>
+  )
 }
