902582641cfe752c6ea2efa95b34258f22ec03d7
Gary Yau <gary.yau@hotmail.com>
2022-05-25 01:09:25 +0900

    changing style and font family

--- pages/components/NoteOperations.js
+++ pages/components/NoteOperations.js
@@ -1,14 +1,14 @@
-import styles from '../../styles/Evernote.module.scss';
+import styles from '../../styles/Evernote.module.scss'
 
 export default function NoteOperations() {
-  return (
-    <>
-      <div className='{styles.btnContainer}'>
-        <button
-          className={styles.button}>
-            Add a New Note
-          </button>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className={styles.btnContainer}>
+                <button
+                    className={styles.button}>
+                    Add a New Note
+                </button>
+            </div>
+        </>
+    )
 }
